abcd28900370ffa65f44a8007da40b0c14886b01
Kadir <kadirincedev@gmail.com>
2020-07-23 15:16:47 +0300

    Create UI Template

--- Distance Calculator/ContentView.swift
+++ Distance Calculator/ContentView.swift
@@ -10,7 +10,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            ZStack {
+                Rectangle()
+                    .edgesIgnoringSafeArea(.top)
+                    .foregroundColor(.gray)
+                Circle()
+                    .frame(width: UIScreen.main.bounds.width * 0.05, height: UIScreen.main.bounds.height * 0.03)
+                    .foregroundColor(.blue).opacity(0.4)
+            }
+            VStack {
+                Button(action: {
+                    //
+                }) {
+                    Image(systemName: "plus")
+                }
+                .font(.title)
+                .padding()
+                .background(Color.red)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .padding(.top, -35)
+
+                Text("Distance Calculator")
+                    .font(.title).bold()
+                Circle()
+                    .frame(width: UIScreen.main.bounds.width * 0.7, height: UIScreen.main.bounds.height * 0.37)
+            }
+            HStack {
+                Spacer()
+                Button(action: {
+                    //
+                }) {
+                    Image(systemName: "gear")
+                        .font(.largeTitle)
+                        .padding()
+                }
+            }
+        }
     }
 }
 
