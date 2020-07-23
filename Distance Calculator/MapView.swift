d1dfd2ee9994e8b8b996c304ca43cc0ece3fdd7c
Kadir <kadirincedev@gmail.com>
2020-07-23 15:39:24 +0300

    Added Red Flag (Point) and Place Title on MapView.

--- Distance Calculator/MapView.swift
+++ Distance Calculator/MapView.swift
@@ -11,6 +11,10 @@ import SwiftUI
 import MapKit
 
 struct MapView: UIViewRepresentable {
+    
+    var centerCoordinates: CLLocationCoordinate2D
+    var mapAnnotations: [MKPointAnnotation]
+    
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
         mapView.setCenter(CLLocationCoordinate2DMake(37.3344728, -122.008715), animated: true)
@@ -18,12 +22,38 @@ struct MapView: UIViewRepresentable {
     }
     
     func updateUIView(_ uiView: MKMapView, context: Context) {
-        //
+        if mapAnnotations.count != uiView.annotations.count{
+            uiView.removeAnnotations(uiView.annotations)
+            uiView.addAnnotations(mapAnnotations)
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var mapViewParent: MapView
+        init(_ parent: MapView) {
+            self.mapViewParent = parent
+        }
+        
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            mapViewParent.centerCoordinates = mapView.centerCoordinate
+        }
+    }
+}
+
+extension MKPointAnnotation{
+    static var applePark: MKPointAnnotation{
+        let annotation = MKPointAnnotation()
+        annotation.title = "Apple Park"
+        annotation.coordinate = CLLocationCoordinate2D.init(latitude: 37.3344728, longitude: -122.008715)
+        return annotation
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(centerCoordinates: MKPointAnnotation.applePark.coordinate, mapAnnotations: [MKPointAnnotation.applePark])
     }
 }
