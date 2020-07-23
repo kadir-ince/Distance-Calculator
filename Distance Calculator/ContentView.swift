5bf4b546187ee4b88610509756220a1b42bf09aa
Kadir <kadirincedev@gmail.com>
2020-07-23 15:03:23 +0300

    Initial Commit

---
+++ Distance Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Distance Calculator
+//
+//  Created by Kadir on 23.07.2020.
+//  Copyright © 2020 Kadir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
